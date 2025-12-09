93229888bcd9c4a79a026128ec3292c41974f3db
unknown <hengipor90@gmail.com>
2025-12-09 21:10:04 +0700

    update project

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+         content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="{{ asset("assets/src/css/style.min.css") }}">
+    </head>
+
+    <body>
+        <nav class="navbar">
+            <div class="container">
+                <div class="d-flex">
+                    <a href="#" class="brand"></a>
+                    <ul class="navbar-nav"></ul>
+                </div>
+            </div>
+        </nav>
+        <main class="main">
+            <section class="sec1">
+                <div class="overlay-container">
+                    <div class="overlay-d-flex"></div>
+                </div>
+            </section>
+        </main>
+
+        <script type="module" src="{{ asset('assets/src/js/script.get.js') }}"></script>
+    </body>
+
+</html>
